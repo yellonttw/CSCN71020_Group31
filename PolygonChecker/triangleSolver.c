eafe6435b77728cc67921b5eacf310ec79e558d0
weiyi <wweng0864@conestogac.on.ca>
2022-11-23 09:57:19 -0500

    fix judgement

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -6,7 +6,7 @@
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
-	if (side1 <= 0 || side2 <= 0 || side3 <= 0) 
+	if ((side1 <= 0 || side2 <= 0 || side3 <= 0) || (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2))
 	{
 		result = "Not a triangle";
 	}
